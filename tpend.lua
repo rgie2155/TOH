a0725ad23168eecf3bcd05e5eb41e30641ab406e
rgie2155 <110311720+rgie2155@users.noreply.github.com>
2022-07-31 12:35:17 -0400

    Update tpend.lua

--- tpend.lua
+++ tpend.lua
@@ -2,20 +2,4 @@
  
     local player = game.Players.LocalPlayer.Character
     player.HumanoidRootPart.CFrame = endzone
-end)
- 
-ss:Button("Get All Items",function()
-    for _,e in pairs(game.Players.LocalPlayer.Backpack:GetDescendants()) do
-        if e:IsA("Tool") then
-        e:Destroy()
-        end
-        end
-        wait() 
-        for _,v in pairs(game.ReplicatedStorage.Gear:GetDescendants()) do
-        if v:IsA("Tool") then
-        local CloneThings = v:Clone()
-        wait()
-        CloneThings.Parent = game.Players.LocalPlayer.Backpack
- 
-        end
-        end
+end
